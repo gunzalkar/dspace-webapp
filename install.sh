c1b23fa4033e0a6fa604ef1e775404944b284d04
Kshitij Gunjalkar <42808290+gunzalkar@users.noreply.github.com>
2024-02-24 14:07:00 +0530

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,17 @@
+apt update && apt install -y nginx gunicorn
+pip3 install flask
+NGINX_CONF="/etc/nginx/sites-enabled/flask_app"
+echo "server {
+    listen 8020;
+
+    location / {
+            proxy_pass http://127.0.0.1:8000;
+            proxy_set_header Host \$host;
+            proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+    }
+}" > $NGINX_CONF
+unlink /etc/nginx/sites-enabled/default
+systemctl restart nginx
+echo "Nginx and Gunicorn have been configured successfully."
+sleep 2
+gunicorn app:app –daemon
